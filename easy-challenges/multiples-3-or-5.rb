3d033b1bc65a1da9231d333538a900678f22295f
Jeniffer Callou <jenifferprc@gmail.com>
2021-10-14 20:42:18 +0200

    added my solution

--- easy-challenges/multiples-3-or-5.rb
+++ easy-challenges/multiples-3-or-5.rb
@@ -5,3 +5,18 @@
 # so that it returns the sum of all the multiples of 3 or 5 below the number 
 # passed in. Additionally, if the number is negative, return 0 (for languages 
 # that do have them). If number is a multiple of both 3 and 5, only count it once.
+
+def solution(number)
+    array = (0...number).to_a
+    sum = 0
+    array.each do |i|
+        if i % 3 == 0
+            sum += i
+        elsif i % 5 == 0
+            sum += i
+        end
+    end
+    return sum
+end
+
+p solution(10)
