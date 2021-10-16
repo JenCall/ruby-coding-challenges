1938f722d0aac42a47cdb765f80fafdbb71362f2
Jeniffer Callou <jenifferprc@gmail.com>
2021-10-16 10:24:24 +0200

    problem solved

--- easy-challenges/convert-to-camel-case.rb
+++ easy-challenges/convert-to-camel-case.rb
@@ -7,5 +7,19 @@
 # to as Pascal case).
 
 def to_camel_case(str)
-    string = 
+    array_of_letters = str.split("")
+    if array_of_letters.include?("_")
+        string = str.split("_")  
+    else array_of_letters.include?("-") 
+        string = str.split("-")
+    end
+    n = 0
+    word = ""
+
+    string.each { |i| n == 0 ? word << i : word << i.capitalize
+        n += 1 
+    }
+    word
 end
+
+p to_camel_case("the-stealth-warrior")
