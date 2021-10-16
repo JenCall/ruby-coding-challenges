c487893b9bd6e2e67bbf2d917aab00be42b287f3
Jeniffer Callou <jenifferprc@gmail.com>
2021-10-16 09:22:15 +0200

    adjusted method

--- easy-challenges/two-to-one.rb
+++ easy-challenges/two-to-one.rb
@@ -4,18 +4,16 @@ def longest(a1, a2)
   a2 = a2.split("")
   new_word = ""
   a1.each do |i|
-    if a1.include?(i)
-      next
-    else
+    unless new_word.include?(i)
       new_word << i
     end
   end
-#   a2.each do |j|
-#     unless a2.include?(j)
-#       new_word << j
-#     end
-#   end
-#   return new_word
+  a2.each do |j|
+    unless new_word.include?(j)
+      new_word << j
+    end
+  end
+  new_word.split("").sort.join
 end
 
 p longest("hello","hola")
