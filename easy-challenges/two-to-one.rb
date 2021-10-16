5f0a9f0dc173f0add43369bb8d00660e45a524c3
Jeniffer Callou <jenifferprc@gmail.com>
2021-10-16 09:40:16 +0200

    add comments

--- easy-challenges/two-to-one.rb
+++ easy-challenges/two-to-one.rb
@@ -7,21 +7,24 @@
 # my solution
 
 def longest(a1, a2)
-  # your code
+  # string into array
   a1 = a1.split("")
   a2 = a2.split("")
+  # new variable to store the letters
   new_word = ""
-
+  # loop into the arrays a1 and a2
   a1.each { |i| 
+    # if a letter "i" is not in the new_word then add to the new word
     unless new_word.include?(i) 
+        # pushing to new_word
         new_word << i 
     end }   
-
+  # same as a1
   a2.each { |j|
     unless new_word.include?(j)
       new_word << j
     end }
-
+  # convert new_word to string (with split) in order to sort and back to string with join     
   new_word.split("").sort.join
 end
 
