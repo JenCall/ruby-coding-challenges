f123ff472200e64e0f44f679e518133923ba00e3
Jeniffer Callou <jenifferprc@gmail.com>
2021-10-16 09:29:25 +0200

    instructions

--- easy-challenges/two-to-one.rb
+++ easy-challenges/two-to-one.rb
@@ -1,3 +1,11 @@
+# Two to one
+
+# Take 2 strings s1 and s2 including only letters from ato z. 
+# Return a new sorted string, the longest possible, containing 
+# distinct letters - each taken only once - coming from s1 or s2.
+
+# my solution
+
 def longest(a1, a2)
   # your code
   a1 = a1.split("")
