66fc56d6390f32e2560b183c702451c49b5646bf
Jeniffer Callou <jenifferprc@gmail.com>
2021-10-20 21:13:46 +0200

    added digits method

--- easy-challenges/narcissistic-number.rb
+++ easy-challenges/narcissistic-number.rb
@@ -17,5 +17,36 @@ def narcissistic?(value)
     # if final sum is equal initial value then true, otherwise false
     value == sum ? true : false
 end
-
 p narcissistic?(5)
+
+# shorter solution
+def narcissistic1?( value )
+    value == value.to_s.chars.map { |x| x.to_i**value.to_s.size }.reduce(:+)
+end
+p narcissistic1?(5)
+
+# interesting solution
+def narcissistic2?(n)
+    n == n.digits.map { |d| d**n.digits.size } .sum
+end
+p narcissistic3?(5)
+
+# digitis method
+
+# 567321.digits
+# 3#=> [1, 2, 3, 7, 6, 5]
+# 4
+# 5567321.digits[3]
+# 6#=> 7
+
+# 0123.digits(8)
+# 3#=> [3, 2, 1]
+# 4
+# 50xabcdef.digits(16)
+# 6#=> [15, 14, 13, 12, 11, 10]
+
+# 123.to_s.chars.map(&:to_i).sum
+# 3#=> 6
+# 4
+# 5123.digits.sum
+# 6#=> 6
