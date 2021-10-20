8288926e12f95f0ba626cf272bbec10a937edfbb
Jeniffer Callou <jenifferprc@gmail.com>
2021-10-20 22:34:34 +0200

    added my solution

--- easy-challenges/playing-with-digits.rb
+++ easy-challenges/playing-with-digits.rb
@@ -11,3 +11,16 @@
 # dig_pow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
 # dig_pow(46288, 3) should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
 
+# first solution
+def dig_pow(n, p)
+    digit = n.to_s.split(//)
+    power_num = [*p..10] # or (p..10).to_a
+    sum = j = 0
+
+    digit.each { |n| sum += n.to_i ** power_num[j] 
+        j += 1 
+    }
+    sum % n != 0 ? -1 : sum / n
+end
+
+p dig_pow(695, 2)
