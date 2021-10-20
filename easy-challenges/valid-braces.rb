6ae01db1cd23dadd8d84b7755fad4a644b5ee8db
Jeniffer Callou <jenifferprc@gmail.com>
2021-10-20 11:25:22 +0200

    inserted more solutions

--- easy-challenges/valid-braces.rb
+++ easy-challenges/valid-braces.rb
@@ -9,21 +9,52 @@
 # All input strings will be nonempty, and will only consist of parentheses, brackets and 
 # curly braces: ()[]{}.
 
+# first try
 def validBraces(braces)
-    opens = ["(", "[","{"]
-    closes = [")", "]","}"]
-    hash_of_symbols = { "(" => 1, ")" => 2, "[" => 3, "]" => 4,  "{" => 5, "}" => 6 }
+    hash_of_symbols = { "(" => 2, ")" => 2, "[" => 3, "]" => 3,  "{" => 4, "}" => 4 }
     braces = braces.split("")
+    values = []
     if braces.length % 2 == 0
-        braces.each do |i| 
-            if hash_of_symbols.include?(i)
-                values << hash_of_symbols[i]
-            end
+        braces.each { |i| values << hash_of_symbols[i] if hash_of_symbols.include?(i) }
+        if values.size == 2 && values[0] == values[1]
+            true
+        elsif values[0] == values[1] && values[2] == values[3]
+            true
+        else
+            array_1 = values.each_slice( (values.size/2.0).round ).to_a[0]
+            array_2 = values.each_slice( (values.size/2.0).round ).to_a[1].reverse()
+            array_1 == array_2 ? true : false
         end
-        p values
     else
         false
     end
 end
+validBraces("[]{}()")
 
-validBraces("([)")
+# clever solution
+def validBraces1 braces, *a
+    braces_set = { "(" => ")", "{" => "}", "[" => "]" }
+    # p s.class -> is a string
+    # p a.class -> is an array - seems to be converted at *a
+    braces.split("").each do |brace|
+        if braces_set[brace]
+            a << brace
+            p a
+        else
+            p a.pop
+            # braces_set[a.pop]
+            return false if braces_set[a.pop] != brace
+        end
+    end
+    p a
+    a.empty?
+end
+
+p validBraces1("{()}")
+
+# shorter solution
+def validBraces2(braces)
+    while braces.gsub!(/\{\}|\[\]|\(\)/, ''); end
+    braces.empty?
+end
+validBraces2("[]{}()")
