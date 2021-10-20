6dfc38f5343f6b6a0568100fbcf4c4584fbb80ae
Jeniffer Callou <jenifferprc@gmail.com>
2021-10-20 21:01:15 +0200

    added comments

--- easy-challenges/narcissistic-number.rb
+++ easy-challenges/narcissistic-number.rb
@@ -4,14 +4,17 @@
 # each raised to the power of the number of digits in a given base. In this Kata, 
 # we will restrict ourselves to decimal (base 10). --> must return true or false.
 
+# first solution
 def narcissistic?(value)
+    # bring number into an array
     values = value.to_s.split(//)
+    # find the array's size
     digits_size = values.length
-    numbers = [1,2,3,4,5,6,7,8,9,10]
+    # create sum variable to sum the results in the loop
     sum = 0
-    values.each do |i|
-        sum += i.to_i ** digits_size
-    end
+    # for loop to calculate the power of it item in the array + sum them
+    values.each { |i| sum += i.to_i ** digits_size }
+    # if final sum is equal initial value then true, otherwise false
     value == sum ? true : false
 end
 
