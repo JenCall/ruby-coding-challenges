5ffd1ac4c41366b09ccb2db24b7e5cbaf04b2fd5
Jeniffer Callou <jenifferprc@gmail.com>
2021-10-20 20:41:47 +0200

    added file + instructions

---
+++ easy-challenges/narcissistic-number.rb
@@ -0,0 +1,11 @@
+# Does my number look big in this?
+
+# A Narcissistic Number is a positive number which is the sum of its own digits, 
+# each raised to the power of the number of digits in a given base. In this Kata, 
+# we will restrict ourselves to decimal (base 10). --> must return true or false.
+
+def narcissistic?(value)
+    # Code me to return true or false
+end
+
+p narcissistic?(value)
