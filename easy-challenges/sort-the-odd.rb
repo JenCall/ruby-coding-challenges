3934b1a3bcac00908b39a1392371c867bee30006
Jeniffer Callou <jenifferprc@gmail.com>
2021-10-21 17:49:18 +0200

    added first solution

--- easy-challenges/sort-the-odd.rb
+++ easy-challenges/sort-the-odd.rb
@@ -8,5 +8,30 @@
 # [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
 def sort_array(source_array)
-    #your code here
+    x_array = Array.new
+    odd = Array.new
+    final_array = Array.new
+
+    source_array.each { |i|
+        if i % 2 == 0
+            x_array << i
+        else
+            x_array << "x"
+            odd << i
+        end
+    }
+
+    j = 0
+
+    x_array.each { |n| 
+        if n == "x" 
+            final_array << odd.sort[j]
+            j += 1
+        else 
+            final_array << n 
+        end 
+    } 
+    final_array
 end
+
+p sort_array([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
