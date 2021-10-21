dc4c020923b468ad10b3f344d54eec4adc3a4c93
Jeniffer Callou <jenifferprc@gmail.com>
2021-10-21 21:38:06 +0200

    added method

--- easy-challenges/find-the-unique-number.rb
+++ easy-challenges/find-the-unique-number.rb
@@ -7,7 +7,16 @@
 # The tests contain some very huge arrays, so think about performance.
 
 def find_uniq(arr)
-    
+    p arr
+    array = arr.each_slice( (arr.size/3.0).round ).to_a
+    if array[0].reduce(0, :+) == array[1].reduce(0, :+)
+        array[2].each { |i| return i if array[0][0] != i }
+    elsif  
+        array[1].each { |i| return i if array[0][0] != i }
+    elsif  
+        array[0].each { |i| return i if array[2][0] != i }
+    end
 end
 
-p find_uniq([1,1,1,1,0])
+p find_uniq([3, 3, 2, 3, 3, 3, 3, 3])
+# find-the-unique-number.rb
