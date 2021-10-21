5eca66f666ed2fd893f7a1950e20f2a73d31dcec
Jeniffer Callou <jenifferprc@gmail.com>
2021-10-21 17:56:52 +0200

    added method explanation

--- easy-challenges/sort-the-odd.rb
+++ easy-challenges/sort-the-odd.rb
@@ -7,6 +7,7 @@
 # [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 # [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
+# first try
 def sort_array(source_array)
     x_array = Array.new
     odd = Array.new
@@ -33,5 +34,32 @@ def sort_array(source_array)
     } 
     final_array
 end
+sort_array([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
 
-p sort_array([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
+# second solution
+def sort_array_2(source_array)
+    array_of_odds = source_array.select(&:odd?).sort
+    source_array.map { |n| n.even? ? n : array_of_odds.shift }
+end
+p sort_array_2([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
+
+# other solutions
+def sort_array(xs)
+    odd = xs.select(&:odd?).sort.each
+    xs.map{ |x| x.odd? ? odd.next : x }
+end
+
+def sort_array(source_array)
+    odds = source_array.select(&:odd?).sort
+    source_array.map { |e| e.odd? ? odds.shift : e }
+end
+
+# Methods
+
+# shift -> 
+# Removes the first element of self and returns it (shifting all other 
+# elements down by one). Returns nil if the array is empty.
+# If a number n is given, returns an array of the first n elements (or less) 
+# just like array.slice!(0, n) does. With ary containing only the remainder 
+# elements, not including what was shifted to new_ary. See also #unshift for 
+# the opposite effect.
