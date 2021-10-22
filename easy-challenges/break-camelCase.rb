0ac49359ef2aa2e7341623750fbac978b024f3b9
Jeniffer Callou <jenifferprc@gmail.com>
2021-10-22 10:27:38 +0200

    added my solution

--- easy-challenges/break-camelCase.rb
+++ easy-challenges/break-camelCase.rb
@@ -1,2 +1,23 @@
 # Break camelCase
 
+# Complete the solution so that the function will break up camel casing, using a space between words.
+# Example
+# "camelCasing"  =>  "camel Casing"
+# "identifier"   =>  "identifier"
+# ""             =>  ""
+
+def solution(string)
+    # complete the function
+    new_string = []
+    string.split("").each do |i| 
+      if i == i.upcase
+        new_string << " "
+        new_string << i
+      else
+        new_string << i
+      end
+    end
+    new_string.join
+end
+
+p solution("camelCase")
