59ec4241601e170cb981ae6b8abd04d54dc2315d
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-04 19:57:43 +0100

    added more solutions

--- easy-challenges/two_sum.rb
+++ easy-challenges/two_sum.rb
@@ -1,23 +1,31 @@
 
-# First solution - Brute Froce - O(n^2) time / O(1) spaec
+# First solution - Bring  values - Brute Froce - O(n^2) time / O(1) spaec
 
 def two_sum(nums, target)
     nums.each { |i| nums.each { |j| return [i, j] if (i + j) == target } }
 end
 
-p two_sum([2,3,5,7], 9)
-
 # Optimized solution - O(n) time / O(n) spaec
-
 def two_sum(nums, target)
-    values_and_indices = {}
+    hash_value_index = {}
     
-    nums.each_with_index do |val, index|
-        required_val = target - val
-        if values_and_indices[required_val]
-            return [index, values_and_indices[required_val]]
+    nums.each_with_index do |value, index|
+        searched_value = target - value
+        if hash_value_index[searched_value]
+            return [index, hash_value_index[searched_value]]
         end
         
-        values_and_indices[val] = index
+        hash_value_index[value] = index
     end
 end
+
+# Bringing the indexes
+def two_sum(nums, target)
+    for i in (0...nums.length) do
+        for j in (i + 1...nums.length) do
+            return [i, j] if (nums[i] + nums[j]) == target
+        end
+    end
+end
+
+p two_sum([2,3,5,7], 9)
