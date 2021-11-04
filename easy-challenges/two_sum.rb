78c5d4d82826b1798fdfae0fe48f37d519300f3d
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-04 19:53:02 +0100

    added two sum quicker solution

--- easy-challenges/two_sum.rb
+++ easy-challenges/two_sum.rb
@@ -7,3 +7,17 @@ end
 
 p two_sum([2,3,5,7], 9)
 
+# Optimized solution - O(n) time / O(n) spaec
+
+def two_sum(nums, target)
+    values_and_indices = {}
+    
+    nums.each_with_index do |val, index|
+        required_val = target - val
+        if values_and_indices[required_val]
+            return [index, values_and_indices[required_val]]
+        end
+        
+        values_and_indices[val] = index
+    end
+end
