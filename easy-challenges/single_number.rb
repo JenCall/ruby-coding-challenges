35649830e140f7503d0bc0c453db5f7ffa0d6989
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-04 20:58:53 +0100

    add solution

--- easy-challenges/single_number.rb
+++ easy-challenges/single_number.rb
@@ -1,2 +1,8 @@
 # Given a non-empty array of integers nums, every element appears twice 
 # except for one. Find that single one.
+
+def single_number(nums)
+    2 * nums.uniq.sum - nums.sum
+end
+
+p single_number([1,1,2,2,3])
