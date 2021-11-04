5a73bbb5b60fc2c830b3e79c4ec6e2da1714f81c
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-04 19:58:49 +0100

    added intructions

--- easy-challenges/two_sum.rb
+++ easy-challenges/two_sum.rb
@@ -1,3 +1,6 @@
+# Given an array of integers nums and an integer target, return indices of 
+# the two numbers such that they add up to target.
+
 
 # First solution - Bring  values - Brute Froce - O(n^2) time / O(1) spaec
 
