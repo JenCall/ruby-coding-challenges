8538c906bf6b0c09a89aa7b24b7f2206c8e44cb2
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-04 21:17:14 +0100

    add reverse string

---
+++ easy-challenges/reverse_string.rb
@@ -0,0 +1,17 @@
+# Write a function that reverses a string. The input string is given as an array of characters s.
+
+def reverse_string(string)
+    j = string.length
+    new_str = []
+    while j > 0
+        new_str << string[j-1]
+        j -=1
+    end
+    new_str
+end
+
+def reverse_string(s)
+    s.reverse
+end
+
+p reverse_string("hello")
