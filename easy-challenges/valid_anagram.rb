9a871db1cfa44778c9328bc309da3053ba18c21a
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-06 20:57:36 +0100

    add solution

--- easy-challenges/valid_anagram.rb
+++ easy-challenges/valid_anagram.rb
@@ -1,3 +1,8 @@
 # Given two strings s and t, return true if t is an anagram of s, 
 # and false otherwise.  -->from LeetCode<--
 
+def is_anagram(s, t)
+    s.split("").sort == t.split("").sort
+end
+
+p is_anagram("hello", "hola")
