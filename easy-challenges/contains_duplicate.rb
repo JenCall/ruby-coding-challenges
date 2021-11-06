56c2ddba38aebd0f86fffc79e4b7188219c9bee3
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-06 20:42:58 +0100

    add instructions

--- easy-challenges/contains_duplicate.rb
+++ easy-challenges/contains_duplicate.rb
@@ -1,3 +1,8 @@
+# Given an integer array nums, return true if any value appears at least twice 
+# in the array, and return false if every element is distinct.
+# LeetCode
+
+#first solution
 def contains_duplicate(nums)
     h = Hash.new(0)
     nums.each do |n|
