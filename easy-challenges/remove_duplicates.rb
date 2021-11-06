a27379bce85313e82a7168b2d1c75de181800fcf
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-06 20:48:57 +0100

    add instructions

--- easy-challenges/remove_duplicates.rb
+++ easy-challenges/remove_duplicates.rb
@@ -1,4 +1,10 @@
+# Given an integer array nums sorted in non-decreasing order, remove the 
+# duplicates in-place such that each unique element appears only once. 
+# The relative order of the elements should be kept the same.
 
+# LeetCode
+
+# First solution
 def remove_duplicates(nums)
     return 0 if nums.empty?
     index = 0
@@ -10,17 +16,14 @@ def remove_duplicates(nums)
     nums.compact!
     nums.length
 end
+p remove_duplicates([1,2,2,2,3,5,5,5])
 
 # def remove_duplicates(nums)
 #     new = nums.uniq!
 #     new.length
 # end
 
-p remove_duplicates([1,2,2,2,3,5,5,5])
-
 # Complexity analysis
-
 # Time complextiy: O(n). Assume that n is the length of array. 
 # Each of i and j traverses at most n steps.
-
 # Space complexity : O(1).
