c4c9ec77e6db3b73d4ec55e71df0c85b11b095d2
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-06 20:46:21 +0100

    add comments

--- easy-challenges/contains_duplicate.rb
+++ easy-challenges/contains_duplicate.rb
@@ -4,12 +4,12 @@
 
 #first solution
 def contains_duplicate(nums)
-    h = Hash.new(0)
+    h = Hash.new(0)                 # hash with value of 0 as integer
     nums.each do |n|
-        h[n] += 1
-        return true if h[n] > 1
+        h[n] += 1                   # adding 1 for each key
+        return true if h[n] > 1     # if a value sums more than 1 then true
     end
-    false
+    false                           # if keys have 1 as value it means it appears only once
 end
 
 p contains_duplicate([1,2,3,4])
