8b0467be065bd8b150f707e2acfe73a60b2deaae
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-06 21:02:58 +0100

    add solution

--- easy-challenges/first_unique_char.rb
+++ easy-challenges/first_unique_char.rb
@@ -1,3 +1,16 @@
 # Given a string s, find the first non-repeating character in it and 
 # return its index. If it does not exist, return -1. --from LeetCode--
 
+# First Solution
+def first_uniq_char(s)
+    hash = {}
+    s.each_char.with_index do |char, index|
+        if hash.key?(char)
+            hash[char] = false 
+        else
+            hash[char] = index 
+        end
+    end
+    hash.each { |char,index| return hash[char] if hash[char] }
+    -1
+end
