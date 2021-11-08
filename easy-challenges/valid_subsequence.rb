304dd3858daa40190e3e67cfa207e31ce321e2b1
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-08 21:53:25 +0100

    refactor solution

--- easy-challenges/valid_subsequence.rb
+++ easy-challenges/valid_subsequence.rb
@@ -7,7 +7,6 @@ def is_subsequence(s, t)
     
     while t_index < t.length && s_index < s.length
         s_index += 1 if t[t_index] == s[s_index]
-
         t_index += 1
     end
     s_index == s.length
