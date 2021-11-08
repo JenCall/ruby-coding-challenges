17bc28134e62e135c913d6b384ac35791b2c54f2
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-08 21:48:33 +0100

    added first solution

--- easy-challenges/valid_subsequence.rb
+++ easy-challenges/valid_subsequence.rb
@@ -1,2 +1,15 @@
 # Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 # Leetcode
+
+def is_subsequence(s, t)
+    t_index = 0
+    s_index = 0
+    
+    while t_index < t.length && s_index < s.length
+        if t[t_index] == s[s_index]
+            s_index += 1
+        end
+        t_index += 1
+    end
+    return s_index == s.length
+end
