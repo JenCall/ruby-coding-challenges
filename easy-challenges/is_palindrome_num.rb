55ead4e7200534cbffa1b9e22b4cd8224af94ae3
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-09 21:37:50 +0100

    still working on solution

--- easy-challenges/is_palindrome_num.rb
+++ easy-challenges/is_palindrome_num.rb
@@ -1,2 +1,18 @@
 # An integer is a palindrome when it reads the same backward as forward. For example, 
 # 121 is palindrome while 123 is not.
+
+def is_palindrome(x)
+    nums= x.to_s.split("")
+    j = 0
+    i = nums.length - 1
+    if nums[j] == nums[i]
+        j += 1
+        i -= 1
+    else 
+        false
+    end
+    
+    
+end
+
+p is_palindrome(121)
