111684c3734fb176896a4f2159953cd86f4a36dd
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-09 21:40:05 +0100

    added solution

--- easy-challenges/is_palindrome_num.rb
+++ easy-challenges/is_palindrome_num.rb
@@ -2,17 +2,7 @@
 # 121 is palindrome while 123 is not.
 
 def is_palindrome(x)
-    nums= x.to_s.split("")
-    j = 0
-    i = nums.length - 1
-    if nums[j] == nums[i]
-        j += 1
-        i -= 1
-    else 
-        false
-    end
-    
-    
+    x.to_s == x.to_s.reverse
 end
 
-p is_palindrome(121)
+p is_palindrome(-121)
