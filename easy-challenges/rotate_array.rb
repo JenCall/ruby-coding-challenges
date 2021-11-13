992ff9d993cc388438c9a8526c1f4370492e95d6
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-13 21:40:13 +0100

    added optimized solutions

--- easy-challenges/rotate_array.rb
+++ easy-challenges/rotate_array.rb
@@ -8,11 +8,32 @@
 # rotate 2 steps to the right: [6,7,1,2,3,4,5]
 # rotate 3 steps to the right: [5,6,7,1,2,3,4]
 
+# First Solution - times
 def rotate(nums, k)
     k.times do
     nums.unshift(nums[-1]).delete_at(-1)
    end 
    nums
 end
-
 p rotate([1,2,3,4], 4)
+
+# Second Solution - while
+def rotate(nums, k)
+    i = 0
+    while i < k do
+    nums.unshift(nums[-1])
+    nums.delete_at(-1)
+    i +=1
+   end 
+   nums
+end
+
+#Optimized in Ruby - rotate
+def rotate(nums, k)
+    nums.rotate!(-k)
+end
+
+# Another Idea
+def rotate(nums, k)
+    k.times {|i| nums.unshift(nums.pop)}
+end
