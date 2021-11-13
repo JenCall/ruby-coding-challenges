4e7d8dee0612adb416a3fdcecc9ed08e19dd5451
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-13 21:35:07 +0100

    added first solution with times

--- easy-challenges/rotate_array.rb
+++ easy-challenges/rotate_array.rb
@@ -7,3 +7,12 @@
 # rotate 1 steps to the right: [7,1,2,3,4,5,6]
 # rotate 2 steps to the right: [6,7,1,2,3,4,5]
 # rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+def rotate(nums, k)
+    k.times do
+    nums.unshift(nums[-1]).delete_at(-1)
+   end 
+   nums
+end
+
+p rotate([1,2,3,4], 4)
