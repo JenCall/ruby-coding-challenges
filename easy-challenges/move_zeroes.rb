a4b04961687ee7b61a0082675a1f0b87670f979c
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-14 19:54:35 +0100

    added first solution with times + each

--- easy-challenges/move_zeroes.rb
+++ easy-challenges/move_zeroes.rb
@@ -2,3 +2,19 @@
 # Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 # Note that you must do this in-place without making a copy of the array.
 
+# First solution
+def move_zeroes(nums)
+    i = 1
+    nums.each do |n|
+        if n == 0
+            nums.delete(n)
+            i += 1
+        end
+    end
+    i.times do
+        nums << 0   
+    end
+    nums
+end
+
+p move_zeroes([0,1,0,2,3,5])
