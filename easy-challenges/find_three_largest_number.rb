a9120fef882ad95d802535592ac2c583f7949242
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-14 20:18:05 +0100

    added instructions

---
+++ easy-challenges/find_three_largest_number.rb
@@ -0,0 +1,4 @@
+# Find Three Largest Numbers
+# Write a function that takes in an array of at least 3 integers and, without
+# sorting the input array, returns a sorted array of the three largest numbers
+# in the input array
