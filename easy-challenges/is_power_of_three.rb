904d80ec773cf9ddd137fe538699d542cba913bc
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-15 22:33:56 +0100

    added solution

--- easy-challenges/is_power_of_three.rb
+++ easy-challenges/is_power_of_three.rb
@@ -1,3 +1,10 @@
 # Is the power of three? --LeetCode--
 # Given an integer n, return true if it is a power of three. Otherwise, return false.
 # An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+def is_power_of_three(n)
+    return true if n == 3 || n == 1
+    return false if n % 3 != 0 || n.zero?
+
+    is_power_of_three(n / 3)
+end
